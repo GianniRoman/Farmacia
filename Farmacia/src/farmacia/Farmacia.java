60d5d6387b9395ab15dcf5503dca458a9ffb9dbc
LautaroPalacin <lautaropalacin2015@gmail.com>
2017-10-02 00:56:07 -0300

    Commit de domingo

--- Farmacia/src/farmacia/Farmacia.java
+++ Farmacia/src/farmacia/Farmacia.java
@@ -7,10 +7,6 @@ public class Farmacia {
   
     public static void main(String[] args) {
         new Login().setVisible(true);
-        ConexionBD nuevaconexion = ConexionBD.getInstance();
-        System.out.println(nuevaconexion.getConexion());
-        ConexionBD otra = ConexionBD.getInstance();
-        System.out.println(otra.getConexion());
         
     }
     
