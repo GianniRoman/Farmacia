2d0df881e35cf837da7576c5d003f29f269470a1
LautaroPalacin <lautaropalacin2015@gmail.com>
2017-10-16 02:44:57 -0300

    comit de viceo2

--- Farmacia/src/Modelos/Medicamento.java
+++ Farmacia/src/Modelos/Medicamento.java
@@ -1,12 +1,17 @@
 package Modelos;
 
+import Conexion.ConexionBD;
 import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Medicamento {
     String marca;
     String nombre;
     float precio;
-    int presentacion;
+    String presentacion;
     Date vencimiento;
     Lote[] lotes;
     Receta receta;
@@ -15,7 +20,149 @@ public class Medicamento {
     Drogueria drogueria;
     Venta venta;
 
+    public String getPresentacion() {
+        return presentacion;
+    }
+
+    public void setPresentacion(String presentacion) {
+        this.presentacion = presentacion;
+    }
+
     public Medicamento() {
     }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    
+
+    public Date getVencimiento() {
+        return vencimiento;
+    }
+
+    public void setVencimiento(Date vencimiento) {
+        this.vencimiento = vencimiento;
+    }
+
+    public Lote[] getLotes() {
+        return lotes;
+    }
+
+    public void setLotes(Lote[] lotes) {
+        this.lotes = lotes;
+    }
+
+    public Receta getReceta() {
+        return receta;
+    }
+
+    public void setReceta(Receta receta) {
+        this.receta = receta;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public Droga[] getDrogas() {
+        return drogas;
+    }
+
+    public void setDrogas(Droga[] drogas) {
+        this.drogas = drogas;
+    }
+
+    public Drogueria getDrogueria() {
+        return drogueria;
+    }
+
+    public void setDrogueria(Drogueria drogueria) {
+        this.drogueria = drogueria;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+
+    public ArrayList<Medicamento> TraerMedicamentosDeVenta(String vcod) {
+        ConexionBD db = ConexionBD.getInstance();
+        ArrayList <Medicamento> aMed = new ArrayList<>();
+        db.Select("lote","ticket","ticket.vcod ="+vcod+"");
+        String[] lotes = new String[50];
+        int[] mcods = new int[50];
+        int i = 0;
+        int iterMed = 0;
+        try {
+            while(db.getRs().next()){
+                lotes[i] = db.getRs().getString("lote");
+                i++;
+                }
+            int cantLotes = lotes.length;
+            i = 0;
+            while(cantLotes > 0){
+               db.Select("mcod","stock","lote ='"+lotes[i]+"'");
+               i++;
+               cantLotes--;
+            while(db.getRs().next()){
+                   mcods[iterMed] = db.getRs().getInt("mcod");
+                   iterMed++;
+               }
+             }
+            int cantMed = mcods.length;
+            i=0;
+            while(cantMed>0)
+            {
+                db.Select("mnbre,mmarca,mpres","medicamento","mcod ='"+mcods[i]+"'");
+                if(db.getRs().next())
+                {
+                    Medicamento aux = new Medicamento();
+                    aux.nombre = db.getRs().getString("mnbre");
+                    aux.marca = db.getRs().getString("mmarca");
+                    aux.presentacion = db.getRs().getString("mpres");
+                    aMed.add(aux);
+                }
+                cantMed--;
+                i++;
+            }             
+            }catch (SQLException ex) {
+            Logger.getLogger(Medicamento.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return aMed;
+    }
+    
+     @Override
+    public String toString() {
+        return nombre+"";
+    }
+   
     
 }
--- Farmacia/src/Modelos/Receta.java
+++ Farmacia/src/Modelos/Receta.java
@@ -3,13 +3,61 @@ package Modelos;
 import java.sql.Date;
 
 public class Receta {
-    Date fecha;
+    String dia,mes,año;
     Medico medico;
     Medicamento[] medicamentos;
     ObraSocial obraSocial;
     Cliente cliente;
 
     public Receta() {
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getAño() {
+        return año;
+    }
+
+    public void setAño(String año) {
+        this.año = año;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Medicamento[] getMedicamentos() {
+        return medicamentos;
+    }
+
+    public void setMedicamentos(Medicamento[] medicamentos) {
+        this.medicamentos = medicamentos;
+    }
+
+    public ObraSocial getObraSocial() {
+        return obraSocial;
+    }
+
+    public void setObraSocial(ObraSocial obraSocial) {
+        this.obraSocial = obraSocial;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
     }
     
     
